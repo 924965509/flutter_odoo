0105b198ffc2f9073323447673228f94159b4bff
YoungHnhb <hnhbliyueyang@gmail.com>
2018-10-29 20:44:33 +0800

    [ADD] add login button & home page

--- lib/login.dart
+++ lib/login.dart
@@ -4,16 +4,19 @@ class LoginPage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _LoginPage();
   }
 }
 
 class _LoginPage extends State<LoginPage> {
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
+      backgroundColor: Color.fromARGB(255, 170, 102, 204),
       body: SafeArea(child: ListView(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         children: <Widget>[
@@ -25,9 +28,50 @@ class _LoginPage extends State<LoginPage> {
               Text('ODOO')
             ],
           ),
-          SizedBox(height: 120.0),
-
-
+          SizedBox(height: 60.0),
+          TextField(
+            decoration: InputDecoration(
+              filled: true,
+              labelText: 'Host',
+              fillColor: Colors.white,
+            ),
+          ),
+          SizedBox(height: 12.0),
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+              filled: true,
+              labelText: 'Username',
+              fillColor: Colors.white,
+            ),
+          ),
+          SizedBox(height: 12.0),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              filled: true,
+              labelText: 'Password',
+              fillColor: Colors.white,
+            ),
+            obscureText: true,
+          ),
+          ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                child: Text('CANCEL'),
+                onPressed: () {
+                  _usernameController.clear();
+                  _passwordController.clear();
+                },
+              ),
+              RaisedButton(
+                child: Text('NEXT'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          )
         ],
       )),
     );
